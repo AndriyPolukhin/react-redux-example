39f1b64c2e49346063210b14691a07b7ad41eab2
AndriyPolukhin <andriy.polukhin@gmail.com>
2020-01-04 21:51:51 +0200

    simple react-redux setup

---
+++ src/store/reducer.js
@@ -0,0 +1,26 @@
+import * as actionTypes from "./actionTypes";
+
+const initialState = {
+  articles: [
+    { id: 1, title: "post 1", body: "Quisque cursus, metus vitae pharetra" },
+    { id: 2, title: "post 2", body: "Quisque cursus, metus vitae pharetra" }
+  ]
+};
+
+const reducer = (state = initialState, action) => {
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case actionTypes.ADD_ARTICLE:
+      const newArticle = {
+        id: Math.random(),
+        title: action.article.title,
+        body: action.article.body
+      };
+      return {
+        ...state,
+        articles: state.articles.concat(newArticle)
+      };
+  }
+  return state;
+};
+export default reducer;
